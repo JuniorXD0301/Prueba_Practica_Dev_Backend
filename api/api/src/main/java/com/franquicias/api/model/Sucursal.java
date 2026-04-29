c04e57530bcb417d22df9270f0f0f00833e9d937
JuniorXD0301 <juniorcorredor2@gmail.com>
2026-04-29 18:11:29 -0500

    Agregar JsonIgnore

--- api/api/src/main/java/com/franquicias/api/model/Sucursal.java
+++ api/api/src/main/java/com/franquicias/api/model/Sucursal.java
@@ -2,6 +2,8 @@ package com.franquicias.api.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -28,6 +30,7 @@ public class Sucursal {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "franquicia_id", nullable = false)
+    @JsonIgnore
     private Franquicia franquicia;
 
     @OneToMany(mappedBy = "sucursal", 
